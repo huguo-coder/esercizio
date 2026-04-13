3bd90f6fa8e71ec08907288b2d8027bf0afaf1b3
huguo-coder <hu.guo@iisgalvanimi.edu.it>
2026-04-13 09:50:01 +0000

    esercize di quarto operazione

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from datetime import datetime
 
 app = FastAPI()
 
-# Spieghiamo a FastAPI che i file dentro "static" sono accessibili
+# Serve per rendere accessibili CSS e JS
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def home():
-    # Restituisce direttamente il file HTML
     return FileResponse('static/home.html')
-
-from datetime import datetime
 
 @app.get("/ora")
 def dammi_ora():
-    # Restituiamo un dizionario (JSON)
     return {"orario": datetime.now().strftime("%H:%M:%S")}
+
+# NUOVO: Endpoint con parametro di query
+@app.get("/saluta")
+def saluta_utente(nome: str):
+    return {"messaggio": f"Ciao {nome}, benvenuto nel server di Terza!"}
+@app.get("/somma")
+def calcola_somma(n1: float, n2: float):
+    risultato = n1 + n2
+    return {"risultato": risultato}
+    import random
+
